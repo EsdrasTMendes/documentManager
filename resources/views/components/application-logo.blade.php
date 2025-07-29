d74e6d85675370a60bdcdfe0ee9e8b6564456f4e
Esdras Tenorio <esdrastmendes@gmail.com>
2025-07-28 23:08:11 -0300

    Feat: fluxo de autenticação criado, blade de Login e ajuste no layout e logo

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center gap-2">
+    <img src="{{ asset('storage/logo.webp') }}" alt="Logo" class="h-14 w-auto" {{ $attributes }} />
+    <img src="{{ asset('storage/logo-midia-simples-texto.webp') }}" alt="Logo texto" class="h-5 w-auto" {{ $attributes }} />
+</div>
