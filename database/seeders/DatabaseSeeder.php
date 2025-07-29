3105095ba270f97a39170434ceab2d62f2b0560b
Esdras Tenorio <esdrastmendes@gmail.com>
2025-07-28 21:38:34 -0300

    Fix: remove código inutilizado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,16 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         $this->call([
            ProductCategoriesTableSeeder::class,
            ProductsTableSeeder::class,
         ]);
-
-//        User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
     }
 }
